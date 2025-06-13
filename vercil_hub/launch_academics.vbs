022dacd78c5fdd8d0b98a4caab9834ab5acbff4b
silvernuke911 <vercilbenben5@gmail.com>
2025-06-13 18:33:57 +0800

    new changes

---
+++ vercil_hub/launch_academics.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "C:\Users\verci\Documents\Python Code\html_code\vercil_hub\open_up.bat", 0
---
+++ vercil_hub/open_up.bat
@@ -0,0 +1,3 @@
+@echo off
+start /min "" "chrome" --profile-directory="Profile 1"
+start "" "C:\Users\verci\Documents\Python Code\html_code\vercil_hub\academics.html"
